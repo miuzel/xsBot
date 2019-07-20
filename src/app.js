8aabb084bbea9fc4674e88b93b5ec1cd33eb72d6
miuzel <miuzel@gmail.com>
2019-07-21 03:41:36 +0800

    update add mention activation

--- src/app.js
+++ src/app.js
@@ -4,12 +4,11 @@
 
 // Extract the required classes from the discord.js module
 import { Client, RichEmbed } from 'discord.js';
-import { token } from '../settings';
+import { token,gProject,prefix,myUsername} from '../settings';
 import { exists } from 'fs';
 import runDialogFlow from './helpers/dialogBot';
 // Create an instance of a Discord client
 const client = new Client();
-
 
 /**
  * The ready event is vital, it means that only _after_ this will your bot start reacting to information
@@ -19,32 +18,24 @@ client.on('ready',  () => {
   console.log('I am ready!');
 });
 
+var tryDialog = async m => {
+  let result = await runDialogFlow(m.author.id,m.content.trim().substr(4),gProject)
+  if(result){
+    m.channel.send(result.response);
+  }
+}
+
+var isMine = m => {
+  console.log(m)
+  return m.content.trim().toLowerCase().startsWith(prefix) || 
+  (m.mentions.users.first() && m.mentions.users.first().username === myUsername);
+} 
+
 client.on('message', message => {
   // If the message is "how to embed"
-  if (message.content === 'how to embed') {
-    // We can create embeds using the MessageEmbed constructor
-    // Read more about all that you can do with the constructor
-    // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
-    const embed = new RichEmbed()
-      // Set the title of the field
-      .setTitle('A slick little embed')
-      // Set the color of the embed
-      .setColor(0xFF0000)
-      // Set the main content of the embed
-      .setDescription('Hello, this is a slick embed!');
-    // Send the embed to the same channel as the message
-    message.channel.send(embed);
-  } else if (message.content.trim().toLowerCase().startsWith('xsjj')) {
-    
-    let tryDialog = async m => {
-      let result = await runDialogFlow(m.author.id,m.content.trim().substr(4),'xsbot-wgmlfx')
-      console.log(result)
-      m.channel.send(result.response);
-    }
+  if (isMine(message)) {
     tryDialog(message);
   }
-
-
 });
 
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
