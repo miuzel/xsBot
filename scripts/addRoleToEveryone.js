7a498743ce0a047536b88ea2496dcc069366b673
miuzel <miuzel@gmail.com>
2019-08-19 13:44:35 +0800

    add emitter limit 0

--- scripts/addRoleToEveryone.js
+++ scripts/addRoleToEveryone.js
@@ -9,7 +9,7 @@ const token = config.token;
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
 client.login(token);
 const [guildName,roleName,addDelete] = process.argv.slice(2);
-
+process.setMaxListeners(0)
 
 client.on('ready',  () => {
     console.log(`I am ready! Updating role ${roleName} of members in ${guildName}`);
